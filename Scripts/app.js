57809baa51c299b62d2fe80cc207a81c372a1e9d
Masatsugu Hirano <masatsugu.hira39@gmail.com>
2022-09-27 04:03:24 -0400

    added saving and loading functions

--- Scripts/app.js
+++ Scripts/app.js
@@ -2,14 +2,46 @@
 //IIFE -- Immmediately Invoked Function Expression
 // AKA - Self Executing Function
 (function () {
+    /**
+     * This method saves our data to localStorage
+     *
+     * @param {any[]} contactList
+     */
+    function SaveContactListData(contactList) {
+        let count = 0;
+        for (const contact of contactList) {
+            let newContact = new Contact(contact.FullName, contact.ContactNumber, contact.EmailAddress);
+            localStorage.setItem(count.toString(), newContact.toJSON());
+            count++;
+        }
+    }
+    /**
+     * This method reads our data from localStorage and returns a Contact Array
+     *
+     * @return {*}  {Contact[]}
+     */
+    function LoadContactListData() {
+        // create an empty Contact Array Container
+        let ContactArray = new Array();
+        let keys = Object.keys(localStorage);
+        for (let key of keys) {
+            let newContact = new Contact();
+            newContact.fromJSON(localStorage.getItem(key));
+            ContactArray.push(newContact);
+        }
+        return ContactArray;
+    }
     // First method of using functions
     function Start() {
         console.log("App Started!");
-        // LoadData("GET", "./Data/contacts.json", function(XHR){
-        //     console.log(XHR);
-        // });
         $.getJSON("./Data/contacts.json", function (DataSource) {
-            console.log(DataSource.ContactList[0]);
+            // Get your data from the DataSource
+            let contactList = DataSource.ContactList;
+            SaveContactListData(contactList);
+            let ContactArray = LoadContactListData();
+            for (const contact of ContactArray) {
+                console.log(contact.toString());
+            }
         });
     }
     window.addEventListener("load", Start);
---
+++ Scripts/contact.ts
@@ -0,0 +1,98 @@
+class Contact
+{
+    // private instance members (fields)
+    private fullName: string;
+    private contactNumber: string;
+    private emailAddress: string;
+
+    // public properties
+    get FullName():string
+    {
+        return this.fullName;
+    }
+
+    set FullName(name: string)
+    {
+        this.fullName = name;
+    }
+
+    get ContactNumber():string
+    {
+        return this.contactNumber;
+    }
+
+    set ContactNumber(contactNumber: string)
+    {
+        this.contactNumber = contactNumber;
+    }
+
+    get EmailAddress():string
+    {
+        return this.emailAddress;
+    }
+
+    set EmailAddress(emailAddress: string)
+    {
+        this.emailAddress = emailAddress;
+    }
+
+
+    // constructor
+    /**
+     *Creates an instance of Contact.
+
+     * @param {string} [fullName="unknown name"]
+     * @param {string} [contactNumber="no contact number"]
+     * @param {string} [emailAddress="unknown email address"]
+     * @memberof Contact
+     */
+    constructor(fullName: string = "unknown name", contactNumber: string = "no contact number", emailAddress: string = "unknown email address")
+    {
+        this.FullName = fullName;
+        this.ContactNumber = contactNumber;
+        this.EmailAddress = emailAddress;
+    }
+
+    // public methods
+    /**
+     * This method overrides the built-in toString method for the Contact class
+     *
+     * @returns {string}
+     * @memberof Contact
+     */
+    public toString(): string
+    {
+        let outputString = "";
+        outputString += `Full Name     : ${this.FullName}\n`;
+        outputString += `Contact Number: ${this.ContactNumber}\n`;
+        outputString += `Email Address : ${this.EmailAddress}\n`;
+        return outputString;
+    }
+
+    /**
+     * This method converts class Data Members to a comma-separated list compatible with JSON
+     *
+     * @returns {string}
+     * @memberof Contact
+     */
+    public toJSON():string
+    {
+        return `${this.FullName},${this.ContactNumber},${this.EmailAddress}`
+    }
+
+    /**
+     * This method reads data from a comma-separated list and assigns it to class Data Members
+     *
+     * @param {Contact} data
+     * @memberof Contact
+     */
+    public fromJSON(data: string):void
+    {
+        let stringArray: string[] = data.split(",");
+        this.FullName = stringArray[0];
+        this.ContactNumber = stringArray[1];
+        this.EmailAddress = stringArray[2];
+    }
+
+    // private methods
+}
